d60c18275c478fd5d3e3c6d34d68bc270fa0d4f9
hetot <shaxzod.murtazayev@mail.ru>
2022-11-26 04:24:38 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile result;
+        BufferedReader reader = null;
+        try (FileInputStream in = new FileInputStream(file.getPath())) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String name = reader.readLine().split(": ")[1];
+            int age = Integer.parseInt(reader.readLine().split(": ")[1]);
+            String email = reader.readLine().split(": ")[1];
+            long phone = Long.parseLong(reader.readLine().split(": ")[1]);
+            result = new Profile(name, age, email, phone);
+        } catch (Exception e) {
+            result = null;
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                result = null;
+            }
+        }
+        return result;
     }
 }
